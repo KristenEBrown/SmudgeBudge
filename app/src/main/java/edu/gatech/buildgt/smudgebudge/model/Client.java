23c4ac1a94b88276a1e25385adf4284665bf95e9
CollinAvidano <collin.avidano@gmail.com>
2019-03-02 13:14:09 -0500

    The worst implementation of a tcp client ever.

---
+++ app/src/main/java/edu/gatech/buildgt/smudgebudge/model/Client.java
@@ -0,0 +1,73 @@
+package edu.gatech.buildgt.smudgebudge.model;
+
+import java.io.IOException;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class Client {
+
+    public static final String TAG = Client.class.getSimpleName();
+    public static final String SERVER_IP = "192.168.1.8"; //server IP address
+    public static final int SERVER_PORT = 25565;
+    private DataOutputStream out;
+    private Socket socket;
+
+    /**
+     * Constructor of the class. OnMessagedReceived listens for the messages received from server
+     */
+    public Client() {
+        new Thread(new ClientThread()).start();
+    }
+
+    /**
+     * Sends the message entered by client to the server
+     *
+     * @param message text entered by client
+     */
+    public void sendMessage(final int message) {
+        try {
+            out.writeInt(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Close the connection and release the members
+     */
+    public void stopClient() {
+        try {
+            socket.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
+
+
+    class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+                socket = new Socket(serverAddr, SERVER_PORT);
+                out = new DataOutputStream(socket.getOutputStream());
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+    }
+}
