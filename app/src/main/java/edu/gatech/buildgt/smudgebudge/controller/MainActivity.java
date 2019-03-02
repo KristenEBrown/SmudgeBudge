e57f75c67c3973e83036fa6dc6d2b245d114927f
cking90 <cking90@gatech.edu>
2019-03-02 10:38:03 -0500

    Add layout of home screen

--- app/src/main/java/edu/gatech/buildgt/smudgebudge/controller/MainActivity.java
+++ app/src/main/java/edu/gatech/buildgt/smudgebudge/controller/MainActivity.java
@@ -1,7 +1,11 @@
-package edu.gatech.buildgt.smudgebudge;
+package edu.gatech.buildgt.smudgebudge.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import edu.gatech.buildgt.smudgebudge.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBegin(View view) {
+        Intent intent = new Intent();
+
     }
 }
